e326b67085ac90dd0a366a1101fada335244063d
Senya <sttephozol@gmail.com>
2026-09-13 13:02:06 +0800

    Update comments in CALL_AVAILABILITY.py
    Created the file to be saved online and seen online for convenience.
    Actual scripts to be added once sorted out.

---
+++ CALL_AVAILABILITY.py
@@ -0,0 +1,8 @@
+#Created for an undisclosed person.
+#I shouldn't even be showing this but no NDA was signed so here it is LOL
+#plus i want that additional contribution points ykyk B)
+#okay how about this:
+#LET ME TRY BRO.
+#CAN WE CALL EACH OTHER?
+
+#VARIABLES, PRINTED INFORMATION, CONDITIONS TBA
